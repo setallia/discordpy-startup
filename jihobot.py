8681512077ddcae1bc59e1b96ea3e42a66dd9db7
setallia <60489472+setallia@users.noreply.github.com>
2020-01-31 07:24:09 +0900

    Update jihobot.py

--- jihobot.py
+++ jihobot.py
@@ -3,13 +3,13 @@ from discord.ext import tasks
 from datetime import datetime 
 
 TOKEN = "NjcyNTM5OTU5NzY0NDUxMzU5.XjNQ2Q.Lxn-VO8c64SYdc0oOzBfUkVDvN0"
-CHANNEL_ID =662647436119900183
+CHANNEL_ID = 662647436119900183
 client = discord.Client()
 
 @tasks.loop(seconds=60)
 async def loop():
     now = datetime.now().strftime('%H:%M')
-    if now == '07:24':
+    if now == '07:25':
         channel = client.get_channel(CHANNEL_ID)
         await channel.send('おはよう')  
 
