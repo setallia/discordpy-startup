1cf0bfb15feb6cd0a1fcddaa7202d55986536e39
setallia <60489472+setallia@users.noreply.github.com>
2020-01-31 07:23:06 +0900

    Create jihobot.py

---
+++ jihobot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import tasks
+from datetime import datetime 
+
+TOKEN = "NjcyNTM5OTU5NzY0NDUxMzU5.XjNQ2Q.Lxn-VO8c64SYdc0oOzBfUkVDvN0"
+CHANNEL_ID =662647436119900183
+client = discord.Client()
+
+@tasks.loop(seconds=60)
+async def loop():
+    now = datetime.now().strftime('%H:%M')
+    if now == '07:23':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('おはよう')  
+
+loop.start()
+client.run(TOKEN)
